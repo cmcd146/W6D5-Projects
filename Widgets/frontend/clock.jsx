f5f1c1032635f2cf0fd6e9be066aedc4cbc6fae4
chris_mcdonald <mcdonaldchristopherkevin@gmail.com>
2018-09-28 15:27:16 -0700

    Build and style a clock

--- Widgets/frontend/clock.jsx
+++ Widgets/frontend/clock.jsx
@@ -3,10 +3,41 @@ import React from 'react';
 class Clock extends React.Component {
   constructor(){
     super();
+    // this.state = { time: `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}` };
+
+    this.state = { date: `${new Date().toLocaleDateString()}`,
+                  time: `${new Date().toTimeString()}` };
   }
 
   render(){
-    return <h1>Clock</h1>;
+    return <div>
+      <h1>Clock</h1>;
+        <div className="clock">
+          <div className="clock-time">
+            <label> Time </label>
+              <h2 id="time">{this.state.time} </h2>
+          </div>
+
+          <div className="clock-date">
+            <label> Date </label>
+              <h2 id="date">{this.state.date} </h2>
+          </div>
+
+        </div>
+    </div>;
+  }
+
+  tick(){
+    this.setState({ time: `${new Date().toTimeString()}`,
+                    date: `${new Date().toLocaleDateString()}`});
+  }
+
+  componentDidMount() {
+    this.int = setInterval(this.tick.bind(this), 1000);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.int);
   }
 
 }
