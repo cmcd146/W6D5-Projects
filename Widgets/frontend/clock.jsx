b500134770f5ef74c6f22c37006b2083e87fa17e
chris_mcdonald <mcdonaldchristopherkevin@gmail.com>
2018-09-28 14:33:26 -0700

    render h1 for clock with react

---
+++ Widgets/frontend/clock.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Clock extends React.Component {
+  constructor(){
+    super();
+  }
+
+  render(){
+    return <h1>Clock</h1>;
+  }
+
+}
+
+
+export default Clock;
